31004c60185f7c729c3550a10d3d9102c1fa35b9
rogerioncosta <rogerioncosta1@gmail.com>
2024-01-25 12:52:07 -0300

    arquivos modificados

--- src/App2.js
+++ src/App2.js
@@ -1,11 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-function App2() {
+// useState: [constante que define o estado, setContante que altera o estado] = useState('valor inicial')
+function App2(){
     const [input, setInput] = useState('');
-    const [tarefas, setTarefas] = useState([
-        'Tarefa1',
-        'Tarefa2'
-    ]);    
+
+    //const [tarefas, setTarefas] = useState(['Tarefa1', 'Tarefa2']); 
+    
+    // ***** component did mount (quando o componente montar na tela faz algo)
+    const [tarefas, setTarefas] = useState(() =>{
+        const tarefasStorage = localStorage.getItem('@tarefa'); 
+        return tarefasStorage ? JSON.parse(tarefasStorage) : [];
+    });
+    /*useEffect(() =>{
+        const tarefasStorage = localStorage.getItem('@tarefa'); // busco se tem algo no localStorage, armazena na const.
+
+        if(tarefasStorage){
+            //se tiver algo, joga denro da useState setTarefas.
+            setTarefas(JSON.parse(tarefasStorage));
+        }
+
+        // ***** component did unmount
+        /* return () =>{
+            // Quando o componente for desmontado (ao sair da página por exemplo), executa o return.
+        }
+
+    }, []);*/
+    // ********************************************************************************
+
+    // ***** component update (passando um parâmetro pra ele, e quando esse parâmetro sofrer alteração ele chama ali dentro)
+    // useEffect: Assim que o componente é renderizado (carregado) na tela, chama a função useEffect. Se houver um valor no array do 2o argumento da arrow function, e ele sofrer alteração, vai chamar a função.
+    useEffect(() =>{
+        localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+    }, [tarefas]);
 
 
     function handleRegister(event){
@@ -34,7 +60,7 @@ function App2() {
             <ul>        
                 {tarefas.map( 
                     tarefa => ( 
-                        <li key={tarefa}>{tarefa}</li> 
+                        <li key={tarefa}> {tarefa} </li> 
                     )
                 )}      
             </ul>
